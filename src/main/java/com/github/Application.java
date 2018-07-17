d35fd7fa3ecfdb71a9d9ace674234a3aa04c164e
leolin <linhouchao@chaoxing.com>
2018-07-17 16:32:42 +0800

    change

--- src/main/java/com/github/Application.java
+++ src/main/java/com/github/Application.java
@@ -2,15 +2,9 @@ package com.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@EnableCaching
 @SpringBootApplication
-@EnableTransactionManagement
-@EnableScheduling
 @EnableEurekaServer
 public class Application {
 
